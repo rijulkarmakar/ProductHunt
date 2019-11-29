ac98d5f2a5fd3e0976b59beada64f726ae1ecf8d
rijulkarmakar <rijul2012.007@gmail.com>
2019-11-29 20:17:19 +0530

    product model and create page added

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request,'products/index.html')
+@login_required
+def create(request):
+    return render(request,'products/CreateProduct.html')
